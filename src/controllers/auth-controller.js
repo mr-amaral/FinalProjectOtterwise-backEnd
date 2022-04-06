a3f545328ca569414bdb5d4d8fb94cfc03110803
Lucas Amaral <lukinhasilvaamaral@gmail.com>
2022-04-06 00:54:40 -0300

    Feature alreadyHas authController

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -6,22 +6,33 @@ import {
 } from "../helpers/utils.js";
 
 export const signup = async (req, reply) => {
-  const { email, password: pass } = req.body;
+  const { email, password: pass, username, name } = req.body;
 
   try {
     const password = await hashPassword(pass);
 
-    const { password: hashedPassword, ...user } = await prisma.user.create({
-      data: {
-        email,
-        password,
+    const alreadyHas = await prisma.user.findMany({
+      where: {
+        OR: [{ email }, { username }],
       },
     });
 
-    reply.send(user);
+    if (alreadyHas.length > 0) {
+      reply.status(400).send(`Nome de usuario ou email já existente`);
+    } else {
+      const { password: hashedPassword, ...user } = await prisma.user.create({
+        data: {
+          email,
+          password,
+          username,
+          name,
+        },
+      });
+      reply.send(user);
+    }
   } catch (error) {
     console.log(error);
-    reply.status(400).send({ error: `User already exists!` });
+    reply.status(400).send({ error: `Erro ao carregar!` });
   }
 };
 
@@ -31,11 +42,11 @@ export const login = async (req, reply) => {
     let user = await prisma.user.findUnique({ where: { email } });
 
     if (!user) {
-      return reply.status(401).send({ error: "Invalid email or password" });
+      return reply.status(401).send({ error: "Email ou senha invalida" });
     }
 
     if (!(await comparePassword(password, user.password))) {
-      return reply.status(401).send({ error: "Invalid email or password" });
+      return reply.status(401).send({ error: "Email ou senha invalida" });
     }
 
     let { password: pass, ...data } = user;
